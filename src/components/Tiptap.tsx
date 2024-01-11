8bf66f6694b42ad5d7f17b93fe7c5a2b5c0f6049
Felipe Silva Vieira <65823376+fesvieira@users.noreply.github.com>
2024-01-11 16:31:01 -0300

    feat(text-editor): implement code block

--- src/components/Tiptap.tsx
+++ src/components/Tiptap.tsx
@@ -1,9 +1,23 @@
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { createLowlight } from "lowlight";
+import js from "highlight.js/lib/languages/javascript";
+
+import "highlight.js/styles/atom-one-dark.css";
+
+const lowlight = createLowlight();
+
+lowlight.register("js", js);
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      CodeBlockLowlight.configure({
+        lowlight,
+      }),
+    ],
     editorProps: {
       attributes: {
         class: "outline-none",
